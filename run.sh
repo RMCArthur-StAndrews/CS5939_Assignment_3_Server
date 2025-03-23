d4a1076482c95e932e464104a4881bdd570c2640
rm417 <rm417@st-andrews.ac.uk>
2025-03-23 10:35:49 +0000

    fixed view DockerFile

--- run.sh
+++ run.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
 
-# Stop and remove existing containers
-docker-compose down
+# Remove existing containers
+docker rm -f webapp_container api_container
 
 # Remove existing images
-docker-compose rm -f
+docker rmi -f webapp:latest api:latest
 
-# Build and start the containers
-docker-compose up --build -d
+# Navigate to the View directory and build the Docker image for the Webapp
+cd View
+docker build -t webapp:latest -f Dockerfile .
 
-# Check if the services started successfully
+# Check if the Webapp build was successful
 if [ $? -eq 0 ]; then
-  echo "Docker containers started successfully."
-  echo "Webapp is accessible at http://localhost:3000"
-  echo "API is accessible at http://localhost:5000"
-else
-  echo "Failed to start Docker containers."
-fi
+  echo "Webapp Docker image built successfully."
+  cd ..
 
-# Run the containers
-docker-compose start
+  # Cleanup old Docker images after building the Webapp
+  ./clean.sh
+
+  # Build the Docker image for the API
+  docker build -t api:latest -f Dockerfile .
+
+  # Check if the API build was successful
+  if [ $? -eq 0 ]; then
+    echo "API Docker image built successfully."
+
+    # Run the Webapp Docker container
+    docker run -d -p 3000:3000 webapp:latest
+
+    # set environment variable for the webapp_container to connect to the api_container once it has started
+
+    if [ $? -eq 0 ]; then
+      echo "Webapp Docker container started successfully."
+      echo "Webapp is accessible at http://localhost:8888"
+
+      # Run the API Docker container
+      docker run -d -p 5000:5000 api:latest
+
+      if [ $? -eq 0 ]; then
+        echo "API Docker container started successfully."
+        echo "API is accessible at http://localhost:5000"
+      else
+        echo "Failed to start API Docker container."
+      fi
+    else
+      echo "Failed to start Webapp Docker container."
+    fi
+  else
+    echo "Failed to build API Docker image."
+  fi
+else
+  echo "Failed to build Webapp Docker image."
+fi
