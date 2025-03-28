8ffcfd23314e76dbe579b107156c962a9cd6cb0e
rm417 <rm417@st-andrews.ac.uk>
2025-03-28 21:11:34 +0000

    updates Dockerfile and the run script

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ docker rmi -f webapp:latest api:latest
 
 # Navigate to the View directory and build the Docker image for the Webapp
 cd View
-docker build -t webapp:latest -f Dockerfile .
+docker build -t webapp:latest -f Dockerfile . --squash
 
 # Check if the Webapp build was successful
 if [ $? -eq 0 ]; then
@@ -22,7 +22,7 @@ if [ $? -eq 0 ]; then
 
   # Build the Docker image for the API
   DOCKER_BUILDKIT=1
-  docker build -t api:latest -f Dockerfile .
+  docker build -t api:latest -f Dockerfile . --squash
 
   # Check if the API build was successful
   if [ $? -eq 0 ]; then
