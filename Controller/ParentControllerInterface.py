fac14c8fdc2025068c81e0ddffb99d58373dc056
rm417 <rm417@st-andrews.ac.uk>
2025-05-08 18:38:34 +0100

    Adjust memory calc so we don't get negative values

--- Controller/ParentControllerInterface.py
+++ Controller/ParentControllerInterface.py
@@ -75,7 +75,9 @@ def after_request(response):
         return response
 
     end_time = time.time()
-    end_memory = cloud_monitor.process.memory_info().vms
+    end_memory = cloud_monitor.process.memory_info().rss
+    if end_memory == 0:
+        end_memory = g.start_memory
     _, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
 
